51204aadcc47f9f2ac5d2566a951cd8818fd8921
DarynaHorobei97 <daryna_horobei1@ukr.net>
2023-01-17 21:52:58 +0200

    Initial commit

---
+++ src/main/java/org/example/Animal.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Animal {
+
+    int weigth;
+
+    public Animal(int weigth) {
+        this.weigth = weigth;
+    }
+
+    void showWeight() {
+        System.out.println(weigth);
+    }
+
+
+    public static void main(String[] args) {
+
+
+        Animal cat = new Animal(4);
+        Animal dog = new Animal(10);
+
+
+        System.out.println(cat == dog);
+
+    }
+
+}
